23acc6c99663b797cbfafc19482da612101279ef
Akif <akuabass11@gmail.com>
2025-04-21 00:33:55 +0400

    constants

---
+++ app/src/main/java/com/example/mova/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.mova.utils
+
+object Constants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_TOKEN =
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmMzMjYxOWJiOTE4MDAyZTViNTZmNDk5Y2NmMzgyNSIsIm5iZiI6MTcyNTAwODM0MS44NTI3MTEsInN1YiI6IjY2ZDA0OTQ2ZDM1ZTU2MTUwYmFhNmU5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wYP7ngWJnJNseEvD8lKFlyhrwzSPa5L6pfaINDh_L3k"
+    const val BASE_IMG_URL = "https://image.tmdb.org/t/p/w500/"
+    const val DEFAULT_MOVIE_IMG =
+        "https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/2e427ce7-b2f7-4c9a-a79b-8c473fadcb55/bfbc3beb-7902-41e6-b139-e6110c8c7983.png"
+}
